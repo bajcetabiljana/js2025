3bd6a81b99e43d62ce3f7f6a6a3071e053311ec3
bajcetabiljana <bajceta.biljana@gmail.com>
2025-03-19 09:55:17 +0100

    Changing data type

--- index.js
+++ index.js
@@ -100,9 +100,27 @@ document.getElementById("mySubmit").onclick = function(){
     header = document.getElementById("h1").textContent = "Hello: " + userName;
 }
 
+//type conversion = change the datatype of a value to a nother (string, nummber, booleans)
 
+let age = window.prompt("Enter your age");
+age = Number(age);
 
+age+=2;
 
+console.log(age, typeof age);
 
+//jos jedan primer
 
+let x = "";
+let y = "";
+let z = "";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+//ovako mozemo da proverimo da li je neko uneo neki podatak u User input, posto je boolean za empty varijablu uvek false
 
