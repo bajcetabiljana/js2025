2c2262d2d6904544a3e2635d85740680e623364b
bajcetabiljana <bajceta.biljana@gmail.com>
2025-04-18 15:23:29 +0200

    Switch and removing coments from whole document

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log(h1);
 console.log(typeof h1);
 
 //Varijable
-/*
+
 let number;
 number = 5;
 console.log(typeof number);
@@ -17,10 +17,10 @@ let boolean = true;
 let boolean2 = false;
 
 console.log("Sajanje dve varijable: prva je" + number + "druga je" + string + "treca je" + boolean);
-*/
 
-//prvi zadatak
-/*
+
+// 1 = prvi zadatak
+
 let fullName = "Biljana Bajceta";
 let age = 48;
 let student = false;
@@ -28,12 +28,11 @@ let student = false;
 document.getElementById("p1").textContent = "Your name is: " + fullName ;
 document.getElementById("p2").textContent = "Your age is: " + age;
 document.getElementById("p3").textContent = "You are a student: " + student;
-*/
 
 
-// Arithmetic operators = operands (valuse, variables, etc.)
+// 2 = Arithmetic operators = operands (valuse, variables, etc.)
 //                      =   operators (+, -, /, *)
-/*
+
 let students = 30;
 console.log(students);
 
@@ -61,7 +60,7 @@ students %= 2;
 // Modulo = Reminder = ostatak / kad se sttudents podeli sa tim brojem daje nam ostatak.
 let ExtraStudents = students ** 3;
 console.log(students);
-*/
+
 
 
 //Operator precedence
@@ -70,10 +69,11 @@ console.log(students);
 2. exponents
 3. multiplications & division & modulo/reminder
 4. addition & subtraction
-
-let result = 1 + 2 * 3 + 4 ** 2;
-console.log(result);
 */
+
+let resultPres = 1 + 2 * 3 + 4 ** 2;
+console.log(resultPres);
+
 
 
 
@@ -97,14 +97,14 @@ console.log(userName);
 */
 
 //profetional way = html textbox
-/*
+
 let userName;
 let header;
-document.getElementById("mySubmit").onclick = function(){
-    userName = document.getElementById("myText").value;
+document.getElementById("submit1").onclick = function(){
+    userName = document.getElementById("text1").value;
     header = document.getElementById("h1").textContent = "Hello: " + userName;
 }
-    */
+   
 
 
 //type conversion = change the datatype of a value to a nother (string, nummber, booleans)
@@ -153,19 +153,21 @@ console.log(circumference);
 
 
 // Kad user ubacuje podatak preko inbox i submita
-/*
-let message;
-document.getElementById("sub").onclick = function(){
-    radius = document.getElementById("input").value;
+const PI = 3.1415926;
+let radius;
+let circumference;
+
+document.getElementById("submit2").onclick = function(){
+    radius = document.getElementById("text2").value;
     radius = Number(radius);
     circumference = 2 * radius * PI;
-    document.getElementById("circelResult").textContent = "Circumference of the circel is:" + circumference + "cm";
+    document.getElementById("result2").textContent = "Circumference of the circel is:" + circumference + "cm";
 };
-*/
+
 
 //Counter
-/*
-let title = document.getElementById("title3").textContent = "Counter";
+
+let title3 = document.getElementById("title3").textContent = "Counter";
 
 const decreaseBt = document.getElementById("decrease");
 const resetBt = document.getElementById("reset");
@@ -188,7 +190,7 @@ resetBt.onclick = function() {
     count = 0;
     countLabel.textContent = count;
 }
-*/
+
 
 //Math =  obezbedjuje da mozemo da koristimo razlicite funkcije koje nam proracunavaju razne matematicke zadatke.
 /*
@@ -213,7 +215,9 @@ Math.ceil(m);
 Math.max(p,t,m);
 Math.min(p,t,m);
 
-//Izbacuje neki rendom number izmedju 0 i 1
+*/
+
+//Izbacuje neki RANDOM number izmedju 0 i 1
 let randomNum = Math.random();
 
 console.log(randomNum);
@@ -230,59 +234,60 @@ random5 = Math.floor(Math.random()  * 6);
 
 let random6 = Math.floor(Math.random()  * 6)+1;
 // sa +1 mu govorimo koji je broj minimum. ako mu kazemo +4, on ce poceti da izbacuje brojeve od 4
-*/
+
 
 
 //RENDOM NUMBER GENERATOR
-/*
-let randomNum = Math.floor(Math.random()  * 6) + 1;
 
-console.log(randomNum);
-*/
+let randomN = Math.floor(Math.random()  * 6) + 1;
+
+console.log(randomN);
+
 //ovo mozemo uraditi i ovako
-/*
+
 let random37;
 let min = 3;
 let max = 7;
 random37 = Math.floor(Math.random()  * (max - min)) + min;
 
 console.log(random37);
-*/
-/*
-const myButton = document.getElementById("myButton");
+
+//DICE GAME
+
+const submit5 = document.getElementById("submit5");
 const Label1 = document.getElementById("label1");
 const Label2 = document.getElementById("label2");
 const Label3 = document.getElementById("label3");
 
-const min = 1;
-const max = 6;
+const minN = 1;
+const maxN = 6;
 let randomNum1;
 let randomNum2;
 let randomNum3;
 
-myButton.onclick = function(){
-    randomNum1 = Math.floor(Math.random() * max)+ min;
-    randomNum2 = Math.floor(Math.random() * max)+ min;
-    randomNum3 = Math.floor(Math.random() * max)+ min;
+submit5.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * maxN)+ minN;
+    randomNum2 = Math.floor(Math.random() * maxN)+ minN;
+    randomNum3 = Math.floor(Math.random() * maxN)+ minN;
     Label1.textContent = randomNum1;
     Label2.textContent = randomNum2;
     Label3.textContent = randomNum3;
 }   
-    */
+
 
 //If statement
-/*
-let age = 13;
 
-if(age >= 18){
+let age6 = 13;
+
+if(age6 >= 18){
     console.log("You are old enough to enter this site.");
 }
 else{
     console.log("You must be 18+ to enter this site.");
 }
-    */
+    
 // if statment super funkcionise sa buleanom..da se proverava da li je nesto tacno ili ne
-/*
+
 let ifStudent = false;
 
 if(ifStudent){
@@ -291,12 +296,12 @@ if(ifStudent){
 else{
     console.log("You are NOT a student!");
 }
-*/
-/*
-let age = 18;
+
+
+let age66 = 18;
 let hasLicense = false;
 
-if(age >= 16){
+if(age66 >= 16){
     console.log("You are old enough to drive");
     if(hasLicense){
         console.log("You have your license!")
@@ -308,68 +313,70 @@ if(age >= 16){
 else{
     console.log("You are NOT old enough to drive")
 }
-    */
-/*
-let age = 18;
+    
 
-if(age>=100){
+let age666 = 18;
+
+if(age666>=100){
     console.log("You are to old to enter this site!.")
 }
-else if(age==0){
+else if(age666==0){
     console.log("You are just born!.")
 }
-else if(age >= 18){
+else if(age666 >= 18){
     console.log("You are old enough to enter this site.");
 }
-else if(age<=0){
+else if(age666<=0){
     console.log("Your age can not be below 0.")
 }
 
 else{
     console.log("You must be 18+ to enter this site.");
 }
-    */
-/*
-let age;
-const result = document.getElementById("result");
-const submit = document.getElementById("submit");
-const myInput = document.getElementById("myInput");
 
-submit.onclick = function(){
 
-    age = myInput.value;
-    age = Number(age);
-    if(age > 100){
-        result.textContent = "You are to old!";
+//Input your age and recive massage acording to input    
+
+const result6 = document.getElementById("result6");
+const submit6 = document.getElementById("submit6");
+const text6 = document.getElementById("text6");
+let age6666;
+
+submit6.onclick = function(){
+
+    age6666 = text6.value;
+    age6666 = Number(age6666);
+    if(age6666 > 100){
+        result6.textContent = "You are to old!";
     }
-    else if(age == 0){
-        result.textContent = "You are just born";
+    else if(age6666 == 0){
+        result6.textContent = "You are just born";
     }
-    else if(age < 0){
-         result.textContent = "It is imposible age";
+    else if(age6666 < 0){
+         result6.textContent = "It is imposible age";
     }
-    else if( age >= 18){
-        result.textContent = "You are en adult";
+    else if( age6666 >= 18){
+        result6.textContent = "You are en adult";
     }
     else{
-        result.textContent = "You are not en adult.";
+        result6.textContent = "You are not en adult.";
     }
 
 }
-    */
+
 
 //Check proprety is checking if radio button or check box are checked
-/*
+
 const checkBox = document.getElementById("myCheckBox");
 const radioVisa = document.getElementById("visaBtn");
 const radioMaster = document.getElementById("masterBtn");
 const radioPayPal = document.getElementById("payPalBtn");
 const submitResult = document.getElementById("submitResult");
 const payResult = document.getElementById("payResult");
-const submit = document.getElementById("submitBt");
+const submit7 = document.getElementById("submitBt");
 
 
-submit.onclick = function(){
+submit7.onclick = function(){
     if(checkBox.checked){
         submitResult.textContent = "You are subscribed!"; 
     }
@@ -390,7 +397,7 @@ submit.onclick = function(){
         payResult.textContent = "You have not checked any of options!"
     }
 }
-    */
+   
 
 // Ternary operator> shortcut to if{} and else{} statemet helps to assign a variable based on condtion 
 // condition ? codeIfTrue : codeIfFalse;
@@ -418,22 +425,131 @@ let message = isStudent ? "You are a stdent" : "You are not a student";
 console.log(message);
 */
 
-//vezba 2
-/*
-const input = document.getElementById("input7");
-const submit = document.getElementById("submit7");
-const result = document.getElementById("result7");
+//vezba 2 Enter number/our recive message acordingly
+
+const text8 = document.getElementById("text8");
+const submit8 = document.getElementById("submit8");
+const result8 = document.getElementById("result8");
 let time;
 
-console.log("submit");
-console.log(submit);
-submit.onclick = function(){
+console.log("submit8");
+console.log(submit8);
+submit8.onclick = function(){
     console.log("pritisnuo dugme");
-    time = input.value;
+    time = text8.value;
     time = Number(time);
-    result.textContent = time <= 12 ?  "Good morning" : "Good afternoon!";
+    result8.textContent = time <= 12 ?  "Good morning" : "Good afternoon!";
 
+}
+   
+
+// Switch = zamena za puno if, ellse statmenta
+/*
+const input = document.getElementById("input8");
+const submit = document.getElementById("submit8");
+const result = document.getElementById("result8");
+let day;
+*/
+
+/*
+submit.onclick = function(){
+    day = input.value;
+    
+    if(day == 1){
+        result.textContent = "Monday";
+    }
+    else if(day ==2){
+        result.textContent = "Thuesday";
+    }
+    else if(day ==3){
+        result.textContent = "Wednesday";
+    }
+    else if(day ==4){
+        result.textContent = "Thursday";
+    }
+    else if(day ==5){
+        result.textContent = "Friday";
+    }
+    else if(day ==6){
+        result.textContent = "Saturday";
+    }
+    else if(day ==7){
+        result.textContent = "Sunday";
+    }
+    else{
+        result.textContent = "You enterd the wrong number";
+    }
 }
     */
 
+//umesto da ovoliko puta imamo if, else statment...ovo mozemo napisat i ovako:
+const text9 = document.getElementById("text9");
+const submit9 = document.getElementById("submit9");
+const result9 = document.getElementById("result9");
+let day9;
 
+console.log(submit9);
+
+submit9.onclick = function(){
+    console.log("Da li me vidis?")
+    
+
+    day9 = text9.value;
+    console.log(day9)
+
+    switch(day9){
+        case "1":
+            result9.textContent("Monday");
+            break;
+        case "2":
+            result9.textContent = "Thusday";
+            break;
+        case "3":
+            result9.textContent = "Wednsday";
+            break;
+        case "4":
+            result9.textContent = "Thursday";
+            break;
+        case "5":
+            result9.textContent = "Friyday";
+            break;
+        case "6":
+            result9.textContent = "Saturday";
+            break;
+        case "7":
+            result9.textContent = "Sunday";
+            break;
+        default:
+            result9.textContent = "You enterd a wrong number";
+        
+    }
+    console.log(result9);
+
+}
+
+//2 zadatak SWICH
+const text10 = document.getElementById("text10");
+const submit10 = document.getElementById("submit10");
+const result10 = document.getElementById("result10");
+
+let testScore;
+
+submit10.onclick = function(){
+
+    testScore = text10.value;
+    testScore = Number(testScore);
+
+    switch(true){
+        case testScore  >= 90:
+            result10.textContent = "Your Grade is: A";
+            break;
+         case testScore >= 60:
+            result10.textContent = "Your Grade is: B";
+            break;
+        case testScore >= 40:
+            result10.textContent = "Your Grade is: C";
+            break;
+        default:
+            result10.textContent = "Your Grade is: D";
+    }
+}
